1d03cf00d75efeeaaf0fde6662943ab0e7e01ea7
GuillaumeLiard <liardg@gmail.com>
2018-12-23 22:02:32 +0100

    clean

--- pages/index.vue
+++ pages/index.vue
@@ -14,27 +14,3 @@
 		}
 	}
 </script>
-
-<style scoped>
-/* .page {
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-}
-.title {
-	height: 10%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-.main {
-	width: 100%;
-	height: 60%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-} */
-
-</style>
