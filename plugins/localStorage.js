feb6c703de1da7e5338ed020eed7fa5879664c0c
GuillaumeLiard <liardg@gmail.com>
2018-12-28 15:23:05 +0100

    upadate localstorage

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -2,6 +2,7 @@ import createPersistedState from 'vuex-persistedstate'
 
 export default ({store}) => {
 	window.onNuxtReady(() => {
+		console.log('store',store)
 		createPersistedState({
 			// key: 'yourkey',
 			// paths: [...]
